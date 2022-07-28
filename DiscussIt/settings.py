834a88b52004867fc2f20fb999d9a42570d522fd
Mustapha Ahmad <18-30gc052@students.unilorin.edu.ng>
2022-07-28 21:57:45 +0100

    Deploying

--- DiscussIt/settings.py
+++ DiscussIt/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -145,6 +146,9 @@ CHANNEL_LAYERS = {
         },
     },
 }
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage" 
+
 
 if not DEBUG:
     import django_heroku
