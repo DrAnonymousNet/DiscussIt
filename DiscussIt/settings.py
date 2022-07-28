62954b4a75ff07d25cf17941e1a6ec5f8edc531a
Mustapha Ahmad <18-30gc052@students.unilorin.edu.ng>
2022-07-28 21:19:48 +0100

    Deploying

--- DiscussIt/settings.py
+++ DiscussIt/settings.py
@@ -74,14 +74,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'DiscussIt.wsgi.application'
 ASGI_APPLICATION = 'DiscussIt.asgi.application'
 
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-'''
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
