3ab0f4250928dafc082710cf480550863bf85244
Mustapha Ahmad <18-30gc052@students.unilorin.edu.ng>
2022-07-28 18:51:43 +0100

    Deploying

--- DiscussIt/urls.py
+++ DiscussIt/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("chat.urls")),
-    path('auth/', include("account.urls"))
+    path('account/', include("account.urls"))
 ]
